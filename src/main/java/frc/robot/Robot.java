b32ac6e5d425fbc00d6c5dc24b50f1c18ed1f312
FRCTeam3234 <79018739+FRCTeam3234@users.noreply.github.com>
2025-01-25 16:37:00 -0500

    Changes autonomous command handling to optional

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import java.util.Optional;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -64,10 +66,9 @@ public class Robot extends TimedRobot {
   public void autonomousInit() {
     autonomousCommand = robotContainer.getAutonomousCommand();
 
-    // schedule the autonomous command (example)
-    if (autonomousCommand != null) {
-      autonomousCommand.schedule();
-    }
+    // Schedule the autonomous command
+    Optional.ofNullable(autonomousCommand)
+      .ifPresent((command) -> command.schedule());
   }
 
   /** This function is called periodically during autonomous. */
@@ -76,13 +77,9 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (autonomousCommand != null) {
-      autonomousCommand.cancel();
-    }
+    // Cancel the autonomous command when teleop starts running.
+    Optional.ofNullable(autonomousCommand)
+      .ifPresent((command) -> command.cancel());
   }
 
   /** This function is called periodically during operator control. */
