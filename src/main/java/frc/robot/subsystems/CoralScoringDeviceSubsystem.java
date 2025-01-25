7ef54d6d38a3412b5a252c90ce73d962de1ff720
CoderLiam926 <LiamVanderJagt@gmail.com>
2025-01-25 16:13:27 -0500

    I forgot to save
    Here is what should've been in the previous commit.
    
    Co-Authored-By: Henyahyah <182578799+Henyahyah@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/CoralScoringDeviceSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralScoringDeviceSubsystem.java
@@ -1,5 +1,83 @@
 package frc.robot.subsystems;
 
-public class CoralScoringDeviceSubsystem {
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+public class CoralScoringDeviceSubsystem extends SubsystemBase {
+
+  /**
+   * It securely grabs the coral that fell from the chute.
+   */
+  public Command grabCoral() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+  
+  /**
+   * Drops the coral onto the reef.
+   */
+  public Command dropCoral() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+  
+  /**
+   * Stops the coral scorer's wheels from spining.
+   */
+  public Command stopScorerSpin() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+  
+ /**
+   * Moves the coral scorer to the correct angle to drop it on the reef.
+   */
+  public Command placeCoralPosition() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+  
+ /**
+   * Moves the coral scorer to the correct angle to grab the coral that fell from the chute.
+   */
+  public Command loadCoralPosition() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+   /**
+   * Checks to see if the coral is correctly loaded on the coral scorer.
+   */
+  public boolean isCoralLoaded() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+   /**
+   * Checks to see if the coral scorer is aligned with the reef.
+   */
+  public boolean isCoralOverReef() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
 }
