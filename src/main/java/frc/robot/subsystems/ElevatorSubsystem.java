10403fa8e62bd13c9da9614cbb79b030ca2f8fac
CoderLiam926 <LiamVanderJagt@gmail.com>
2025-01-25 15:37:48 -0500

    Elevator Skeleton
    I added empty functions for every elevator action we need to do.
    
    Co-Authored-By: Henyahyah <182578799+Henyahyah@users.noreply.github.com>

---
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -0,0 +1,103 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ElevatorSubsystem extends SubsystemBase {
+
+  /**
+   * Moves the elevator to the home position which is the lowest position.
+   */
+  public Command elevatorHome() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  /**
+   * Moves the elevator to the L2 position.
+   */
+  public Command elevatorL2() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+  
+  /**
+   * Moves the elevator to the L3 position.
+   */
+  public Command elevatorL3() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+  
+  /**
+   * Moves the elevator to the L4 position.
+   */
+  public Command elevatorL4() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+  
+  /**
+   * Moves the dealgaer into the dealgae position.
+   */
+  public Command dealgaeExtend() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+  
+  /**
+   * Moves the dealgaer into the stored position.
+   */
+  public Command dealgaeRetract() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+  
+  /**
+   * Activates the dealgaer.
+   */
+  public Command dealgaeStartSpin() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+  
+  /**
+   * Deactivates the dealgaer.
+   */
+  public Command dealgaeStopSpin() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+}
