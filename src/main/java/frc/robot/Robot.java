db828ccff05fda20e7adf134882a835d555caa22
Jalasta <jalasta20xx@gmail.com>
2025-01-27 11:14:51 -0500

    Should probably initialize the Optional to empty

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,7 +17,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * project.
  */
 public class Robot extends TimedRobot {
-  private Optional<Command> autonomousCommand;
+  private Optional<Command> autonomousCommand = Optional.empty();
 
   private RobotContainer robotContainer;
 
