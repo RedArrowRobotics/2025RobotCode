6d74f8cdd1e1df6ff9af704b710c9fb17e3d21e4
Jalasta <jalasta20xx@gmail.com>
2025-01-27 12:49:39 -0500

    some formatting

--- src/main/java/frc/robot/ControlInputs.java
+++ src/main/java/frc/robot/ControlInputs.java
@@ -19,17 +19,14 @@ public class ControlInputs {
     public static record DriveStickState(double x,double y,double rotation) {}
 
     public DriveStickState getDriveStick() {
-        /**
-         * 
-         */
         // Multipliers for the drive stick axes
         final double driveStickLinearMultiplier = 0.8;
         final double driveStickRotationMultiplier = 0.5;
 
         // Get joystick values
-        var x = ( driveStick.getLeftX() * Math.abs(driveStick.getLeftX()) ) * driveStickLinearMultiplier;
-        var y = ( driveStick.getLeftY() * Math.abs(driveStick.getLeftY()) ) * driveStickLinearMultiplier;
-        var rotation = ( driveStick.getRightX() * Math.abs(driveStick.getRightX()) ) * driveStickRotationMultiplier;
+        var x = ( driveStick.getLeftX() * Math.abs( driveStick.getLeftX()) ) * driveStickLinearMultiplier;
+        var y = ( driveStick.getLeftY() * Math.abs( driveStick.getLeftY()) ) * driveStickLinearMultiplier;
+        var rotation = ( driveStick.getRightX() * Math.abs( driveStick.getRightX()) ) * driveStickRotationMultiplier;
         // Compose the seperate components into a state record
         return new DriveStickState(x, y, rotation);
     }
