52da1073c5bb8eb2ec71790cf2c7c2ddf89675b8
Jalasta <jalasta20xx@gmail.com>
2025-01-28 08:55:22 -0500

    a few more comments

--- src/main/java/frc/robot/ControlInputs.java
+++ src/main/java/frc/robot/ControlInputs.java
@@ -8,11 +8,14 @@ public class ControlInputs {
     // Singleton Instance
     private static ControlInputs instance;
 
-    // Joysticks IDs
+    // Joysticks
     private final XboxController driveStick = new XboxController(0);
     private final Joystick componentsBoardLeft = new Joystick(1);
     private final Joystick componentsBoardRight = new Joystick(2);
 
+    /**
+     * Holds unitless multipliers on linear and angular velocity derived from joystick input.
+     */
     public static record DriveStickState(double x,double y,double rotation) {}
 
     public DriveStickState getDriveStick() {
@@ -20,7 +23,7 @@ public class ControlInputs {
         final double driveStickLinearMultiplier = 0.8;
         final double driveStickRotationMultiplier = 0.5;
 
-        // Get joystick values
+        // Derive joystick values. Inputs are squared to make precice control at low speeds easier
         var x = ( driveStick.getLeftX() * Math.abs( driveStick.getLeftX()) ) * driveStickLinearMultiplier;
         var y = ( driveStick.getLeftY() * Math.abs( driveStick.getLeftY()) ) * driveStickLinearMultiplier;
         var rotation = ( driveStick.getRightX() * Math.abs( driveStick.getRightX()) ) * driveStickRotationMultiplier;
