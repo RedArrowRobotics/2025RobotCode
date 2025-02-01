d7adee65c0170c9c410a5dbc4094e999ccb0cf1c
FRCTeam3234 <79018739+FRCTeam3234@users.noreply.github.com>
2025-02-01 11:47:11 -0500

    wip

--- src/main/java/frc/robot/commands/AlignToReef.java
+++ src/main/java/frc/robot/commands/AlignToReef.java
@@ -3,13 +3,17 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.LimelightHelpers;
+import frc.robot.subsystems.LimelightHelpers.RawFiducial;
+
 import java.util.Optional;
 
 public class AlignToReef extends Command {
     private final DriveSubsystem driveSubsystem;
     private final Optional<Alliance> alliance;
+    private Integer targetFiducialId = null;
 
     public AlignToReef(DriveSubsystem subsystem) {
         driveSubsystem = subsystem;
@@ -20,24 +24,35 @@ public class AlignToReef extends Command {
     @Override
     public void initialize() {
         if(LimelightHelpers.getTV("limelight")) {
+          var fiducials = LimelightHelpers.getRawFiducials("limelight");
+          var ids = Constants.redReefATags;            
             if(alliance.get() == Alliance.Blue) {
-                
-            } else if(alliance.get() == Alliance.Red) {
-
+              ids = Constants.blueReefATags;
+            }
+            RawFiducial closestFiducial = null;
+            for (var fiducial : fiducials) {
+                if (ids.contains(fiducial.id) && (closestFiducial == null || fiducial.distToRobot < closestFiducial.distToRobot)) {
+                  closestFiducial = fiducial;
+                }
+            }
+            if (closestFiducial != null) {
+              targetFiducialId = closestFiducial.id;
             }
         }
       
-      LimelightHelpers.getRawFiducials("limelight");
     }
 
     @Override
     public void execute() {
-
+      if (targetFiducialId == null) {
+        return;
+      }
+      
     }
   
   
     @Override
     public boolean isFinished() {
-      return true;
+      return targetFiducialId == null;
     }
 }
