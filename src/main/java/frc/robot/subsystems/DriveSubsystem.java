6bf2ff0694a9a588cf6d7d925f8115f851f1838a
FRCTeam3234 <79018739+FRCTeam3234@users.noreply.github.com>
2025-02-01 16:37:19 -0500

    command test

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -5,6 +5,8 @@ import com.pathplanner.lib.config.PIDConstants;
 import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.MetersPerSecond;
 
 import java.io.File;
@@ -20,9 +22,11 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import swervelib.parser.SwerveParser;
 import swervelib.SwerveDrive;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.units.measure.LinearVelocity;
 import swervelib.telemetry.SwerveDriveTelemetry;
 import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
+import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 
 public class DriveSubsystem extends SubsystemBase {
@@ -109,10 +113,37 @@ public class DriveSubsystem extends SubsystemBase {
     }
   }
 
-  public Command alignToReefLeft() {
-    return this.run(() -> {
-        
-    });
+  public Command pidDrive(Pose2d target) {
+    var command = new Command() {    
+      private ProfiledPIDController feedback_x;
+      private ProfiledPIDController feedback_y;
+      private ProfiledPIDController feedback_theta;
+
+      @Override
+      public void initialize() {
+        feedback_x = new ProfiledPIDController(0.1, 0, 0, new Constraints(1.0,0.1));
+        feedback_y = new ProfiledPIDController(0.1, 0, 0, new Constraints(1.0,0.1));
+        feedback_theta = new ProfiledPIDController(0.1, 0, 0, new Constraints(1.0,0.1));
+      }
+      @Override
+      public void execute() {        
+        // Get velocity multipliers from PID controller
+        var vx = feedback_x.calculate(getPose().getX(),target.getX());
+        var vy = feedback_y.calculate(getPose().getY(),target.getY());
+        var vtheta = feedback_theta.calculate(getPose().getRotation().getRadians(),target.getRotation().getRadians());
+        // Drive using calculated velocity
+        manualDrive(new DrivePower(vx, vy, vtheta), DriveOrientation.ROBOT_CENTRIC);
+      }
+      @Override
+      public boolean isFinished() {
+        var current = getPose();
+        return current.getMeasureX().isNear(target.getMeasureX(), Inches.of(0.5))
+          && current.getMeasureY().isNear(target.getMeasureY(), Inches.of(0.5))
+          && current.getRotation().getMeasure().isNear(target.getRotation().getMeasure(), Degrees.of(5));
+      }
+    };
+    command.addRequirements(this);
+    return command;
   }
 
   @Override
