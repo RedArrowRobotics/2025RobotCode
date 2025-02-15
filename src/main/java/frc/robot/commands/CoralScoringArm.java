097dcd1b12fef9081909d2b68c3d2c922904f7a4
FRCTeam3234 <79018739+FRCTeam3234@users.noreply.github.com>
2025-02-15 14:23:16 -0500

    Added trigger to see when arm is in position and the limit switch is triggered

--- src/main/java/frc/robot/commands/CoralScoringArm.java
+++ src/main/java/frc/robot/commands/CoralScoringArm.java
@@ -2,10 +2,36 @@ package frc.robot.commands;
 
 import frc.robot.Constants;
 import frc.robot.subsystems.CoralScoringDeviceSubsystem;
+import frc.robot.subsystems.CoralScoringDeviceSubsystem.CoralArmPosition;
 
 public class CoralScoringArm extends PIDCommand {
-    public CoralScoringArm(CoralScoringDeviceSubsystem subsystem, double position) {
-        super(0.0, 0.0, 0.0, subsystem.scorerTilter, position, 10.0);
+    private CoralScoringDeviceSubsystem subsystem;
+    private CoralArmPosition target;
+
+    public CoralScoringArm(CoralScoringDeviceSubsystem subsystem, CoralArmPosition target) {
+        super(0.5, 0.0, 0.0, 
+        subsystem.scorerTilter, 
+        switch(target) {
+            case HOME -> Constants.scorerTilterLoadingPosition;
+            case L2L3 -> Constants.scorerTilterScoringPositionL2L3;
+            case L4 -> Constants.scorerTilterScoringPositionL4;
+            case NONE -> Constants.scorerTilterLoadingPosition;
+         }, 
+         10.0);
+        this.target = target;
+        this.subsystem = subsystem;
         addRequirements(subsystem);
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+        subsystem.coralArmPosition = CoralArmPosition.NONE;
+    }
+
+    @Override
+    public void finalize() {
+        super.finalize();
+        subsystem.coralArmPosition = target;
     }
 }
--- src/main/java/frc/robot/subsystems/CoralScoringDeviceSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralScoringDeviceSubsystem.java
@@ -6,21 +6,26 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants;
 import frc.robot.commands.CoralScoringArm;
-import frc.robot.commands.PIDCommand;
 
 public class CoralScoringDeviceSubsystem extends SubsystemBase {
   SparkMax intakeWheels = new SparkMax(Constants.intakeWheelsMotorID, MotorType.kBrushed);
   public SparkMax scorerTilter = new SparkMax(Constants.coralScorerTilterMotorID, MotorType.kBrushed);
   private DigitalInput coralSensor = new DigitalInput(Constants.coralSensorChannel);
   private DigitalInput reefSensor = new DigitalInput(Constants.reefSensorChannel);
-  public final Trigger reefTrigger = new Trigger(reefSensor::get);
+  public final Trigger reefTrigger = new Trigger(() -> {return armIsInPosition() && isCoralOverReef();});
+  public CoralArmPosition coralArmPosition = CoralArmPosition.HOME;
+
+  public enum CoralArmPosition {
+    HOME,
+    L2L3,
+    L4,
+    NONE
+  }
   /**
    * It securely grabs the coral that fell from the chute.
    * Sucks the coral down with wheels to hold it in place.
@@ -64,18 +69,18 @@ public class CoralScoringDeviceSubsystem extends SubsystemBase {
    * Moves the coral scorer to the correct angle to drop it on the reef.
    */
   public Command placeCoralPositionL2L3() {
-      return new CoralScoringArm(this, Constants.scorerTilterScoringPositionL2L3);
+      return new CoralScoringArm(this, CoralArmPosition.L2L3);
   }
 
   public Command placeCoralPositionL4() {
-    return new CoralScoringArm(this, Constants.scorerTilterScoringPositionL4);
+    return new CoralScoringArm(this, CoralArmPosition.L4);
   }
   
  /**
    * Moves the coral scorer to the correct angle to grab the coral that fell from the chute.
    */
   public Command loadCoralPosition() {
-      return new CoralScoringArm(this, Constants.scorerTilterLoadingPosition);
+      return new CoralScoringArm(this, CoralArmPosition.HOME);
   }
 
    /**
@@ -91,6 +96,11 @@ public class CoralScoringDeviceSubsystem extends SubsystemBase {
   public boolean isCoralOverReef() {
     return reefSensor.get();
   }
+
+  public boolean armIsInPosition() {
+    return coralArmPosition == CoralArmPosition.L2L3 || coralArmPosition == CoralArmPosition.L4;
+  }
+
   @Override
   public void initSendable(SendableBuilder builder) {
     super.initSendable(builder);
