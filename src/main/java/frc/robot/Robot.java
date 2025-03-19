29f1a00cff52a617172acc1c72cd3a047d9aaabe
Jalasta <jalasta20xx@gmail.com>
2025-03-18 20:34:42 -0400

    if we error on robot initialization, don't crashloop

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,97 +10,92 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-  private Optional<Command> autonomousCommand = Optional.empty();
+    private Optional<Command> autonomousCommand = Optional.empty();
 
-  private RobotContainer robotContainer;
+    private RobotContainer robotContainer;
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
-  @Override
-  public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    try {
-      robotContainer = new RobotContainer();
-    } catch (Exception e) {
-      e.printStackTrace();
+    /**
+     * This function is run when the robot is first started up and should be used for any
+     * initialization code.
+     */
+    @Override
+    public void robotInit() {
+        try {
+            robotContainer = new RobotContainer();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    
-  }
 
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-    robotContainer.robotPeriodic();
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
-  }
+    /**
+     * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+     * that you want ran during disabled, autonomous, teleoperated and test.
+     *
+     * <p>
+     * This runs after the mode specific periodic functions, but before LiveWindow and
+     * SmartDashboard integrated updating.
+     */
+    @Override
+    public void robotPeriodic() {
+        if (robotContainer != null) {
+            robotContainer.robotPeriodic();
+        }
+        CommandScheduler.getInstance().run();
+    }
 
-  /** This function is called once each time the robot enters Disabled mode. */
-  @Override
-  public void disabledInit() {}
+    /** This function is called once each time the robot enters Disabled mode. */
+    @Override
+    public void disabledInit() {}
 
-  @Override
-  public void disabledPeriodic() {}
+    /** This function is called periodically during autonomous. */
+    @Override
+    public void disabledPeriodic() {}
 
-  /** This autonomous runs the autonomous command selected by your {@link container} class. */
-  @Override
-  public void autonomousInit() {
-    robotContainer.resetGyro();
-    // If we have an autonomous command selected, schedule it to be ran.
-    autonomousCommand = robotContainer.getAutonomousCommand();
-    autonomousCommand.ifPresent(Command::schedule);
-  }
+    /** This autonomous runs the autonomous command selected by your {@link container} class. */
+    @Override
+    public void autonomousInit() {
+        if (robotContainer != null) {
+            robotContainer.resetGyro();
+            // If we have an autonomous command selected, schedule it to be ran.
+            autonomousCommand = robotContainer.getAutonomousCommand();
+            autonomousCommand.ifPresent(Command::schedule);
+        }
+    }
 
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {}
+    /** This function is called periodically during autonomous. */
+    @Override
+    public void autonomousPeriodic() {}
 
-  @Override
-  public void teleopInit() {
-    // If we were running an autonomous command, cancel it when teleop starts.
-    autonomousCommand.ifPresent(Command::cancel);
-  }
+    /** This function is called once each time the robot enters Teleop mode. */
+    @Override
+    public void teleopInit() {
+        // If we were running an autonomous command, cancel it when teleop starts.
+        autonomousCommand.ifPresent(Command::cancel);
+    }
 
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {
-   robotContainer.teleopPeriodic();
-  }
+    /** This function is called periodically during operator control. */
+    @Override
+    public void teleopPeriodic() {
+        robotContainer.teleopPeriodic();
+    }
 
-  @Override
-  public void testInit() {
-    // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
-  }
+    /** This function is called once each time the robot enters Test mode. */
+    @Override
+    public void testInit() {
+        // Cancels all running commands at the start of test mode.
+        CommandScheduler.getInstance().cancelAll();
+    }
 
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
+    /** This function is called periodically during test mode. */
+    @Override
+    public void testPeriodic() {}
 
-  /** This function is called once when the robot is first started up. */
-  @Override
-  public void simulationInit() {}
+    /** This function is called once when the robot is first started up. */
+    @Override
+    public void simulationInit() {}
 
-  /** This function is called periodically whilst in simulation. */
-  @Override
-  public void simulationPeriodic() {}
+    /** This function is called periodically whilst in simulation. */
+    @Override
+    public void simulationPeriodic() {}
 }
